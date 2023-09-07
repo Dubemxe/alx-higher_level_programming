5557246e66f7a796f7d53821b2d72241802eae98
Chidubem Uchendu <dubemxe@gmail.com>
2023-09-07 09:59:08 -0700

    code updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,10 +9,10 @@ if num_args == 0:
     print("0 arguments.")
 
 elif num_args == 1:
-    print("1 arguments:")
+    print("1 argument:")
     print("1: {}".format(sys.argv[1]))
 else:
     print("{} arguments:".format(num_args))
-#print the list 
+# print the list
     for i in range(num_args):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
