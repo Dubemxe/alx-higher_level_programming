9e7a27f73cd6d5a0aca3ac9ac141c3a7633a2be9
Chidubem Uchendu <dubemxe@gmail.com>
2023-09-07 07:50:49 -0700

    argument listing

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+# get number of arguments
+num_args = len(sys.argv) - 1
+
+if num_args == 0:
+    print("0 arguments.")
+
+elif num_args == 1:
+    print("1 arguments:")
+    print("1: {}".format(sys.argv[1]))
+else:
+    print("{} arguments:".format(num_args))
+#print the list 
+    for i in range(num_args):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
