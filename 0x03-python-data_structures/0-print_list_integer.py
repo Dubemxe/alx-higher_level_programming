2822d322ad2fd82acd451e88a58a62cd6e277dc1
Chidubem Uchendu <dubemxe@gmail.com>
2023-09-09 10:07:25 -0700

    print all intergers

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    if my_list:
+                for item in my_list:
+                    item = int(item)
+                    for i in range(1):
+                        print("{}".format(item))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+Learning and working on Data structres tuple and lists #pythoniscool
