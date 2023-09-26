45686567e0854d7a274a134f9dfc0c90b5583405
Chidubem Uchendu <dubemxe@gmail.com>
2023-09-26 08:17:31 -0700

    with size

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Defines a square'''
 
+
 class Square:
     '''Prints an empty class'''
     def __init__(self):
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Define a square class'''
+
+
+class Square:
+    '''A square class with size'''
+    def __init__(self, size):
+        '''initialize a new square'''
+        self.__size = size
