ded61543c7da2a1525126c2f1b507686c967e0ca
Chidubem Uchendu <dubemxe@gmail.com>
2023-10-09 06:05:51 -0700

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+'''Define a function to return a list of available attributes and methods'''
+
+
+def lookup(obj):
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,4 @@
+Learning inheritance of classes and methods,
+
+putting it to code
+testing for all possible edge cases. #Pythonisawesome
