0fd34b0ab7f1b92434a892f9f1a23b3a4f2f6be7
Chidubem Uchendu <dubemxe@gmail.com>
2023-10-10 09:50:43 -0700

    testing

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Defines a write to file function'''
+
+
+def write_file(filename="", text=""):
+    '''Write a string to a file
+
+    Args:
+        filename - file to write in
+        text - string to wite in filename
+    Return:
+        the number of characters written'''
+    count = 0
+    with open(filename, mode="w", encoding="utf-8") as my_file:
+            count = my_file.write(text)
+    return count
