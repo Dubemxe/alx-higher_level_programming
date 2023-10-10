83640db10c8b069e748efd3f9184ec163d8c83a0
Chidubem Uchendu <dubemxe@gmail.com>
2023-10-10 09:03:12 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,8 @@
 '''Defines a file reading function'''
 
 
-def read_file(filename="my_file_0.txt"):
+def read_file(filename=""):
     '''Open a file with the read-only permission'''
     with open(filename, encoding="utf-8") as my_file:
-        print(my_file.read())
+        for line in my_file:
+            print(line, end="")
