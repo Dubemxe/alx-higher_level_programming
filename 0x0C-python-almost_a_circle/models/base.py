66186aaaffb2ceb8daf3ece3728b2d3b9457ff9f
Chidubem Uchendu <dubemxe@gmail.com>
2023-10-16 04:22:16 -0700

    Base.py completed for task 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,9 +31,39 @@ class Base:
         '''Save a JSON string to a file'''
         if list_objs is None:
             list_objs = []
-        filename = cls.__name__  + '.json'
+        filename = cls.__name__ + '.json'
 
-        json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+        json_string = cls.to_json_string([obj.to_dictionary()
+                                         for obj in list_objs])
 
         with open(filename, 'w') as jsonfile:
             jsonfile.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''Returns the list of the JSON string representation'''
+        if json_string is None or json_string == 0:
+            return []
+        return (json.loads(json_string))
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''Return an instance with all attributes already set'''
+        if dictionary and dictionary != {}:
+            cls.__name__ = "Rectangle"
+            new = cls(1, 1)
+        else:
+            new = cls(1)
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        '''Return a list of instances from a JSON string'''
+        filename = cls.__name__ + '.json'
+        try:
+            with open(filename, "r") as jsonfile:
+                dict_list = Base.from_json_string(jsonfile.read())
+                return [cls.create(**dic) for dic in dict_list]
+        except IOError:
+            return []
