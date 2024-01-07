e526b84357e8cf7a889900f72a9f8e97f40600e2
Chidubem Uchendu <dubemxe@gmail.com>
2024-01-07 06:51:42 -0800

    Code update

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' This script fetches https://alx-intranet.hbtn.io/status site using
+resquests.'''
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    txt = req.text
+
+    print("Body response:")
+    print("\t- type: {}".format(type(txt)))
+    print("\t- content: {}".format(txt))
