0ed22689b79cfc2b9630c1fa8b981de44d1858af
Chidubem Uchendu <dubemxe@gmail.com>
2024-01-30 11:49:32 -0800

    Done!

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed_tasks = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed_tasks === true) {
+        if (completed_tasks[task.userId] === undefined) {
+          completed_tasks[task.userId] = 1;
+        } else {
+          completed_tasks[task.userId]++;
+        }
+      }
+    }
+    console.log(completed_tasks);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
+});
